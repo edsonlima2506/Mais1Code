fcc076a17f151f0136cc8f1056367432911e8a93
Leonnardh <wellington.fmor@gmail.com>
2023-04-18 21:40:03 -0300

    revisão dos erros atuais

--- aula32/script.js
+++ aula32/script.js
@@ -4,24 +4,25 @@ const countryInfoContainer = document.getElementById('countryInfoContainer');
 
 
 searchButton.addEventListener('click', async() =>{
-const Countryname = countryInput.Value;
+const countryName = countryInput.value;
 
-const response = await fetch('https://restcountries.com/v3.1/name/($countryName)');
+const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
 const data =await response.json();
 
-if(data.lenght>0){
-    const country = data = [0];
-    const [name, capital, region, population, flags] = country;
-    countryInfoContainer.innerHTML =
-    '<h2>Informações do País</h2>'
-    '<ul>
-        <li><strong>Nome:</strong>[$name.commom]</li>
-        <li><strong>capital:</strong>[$capital]</li>
-        <li><strong>Região:</strong>[$region]</li>
-        <li><strong>População:</strong>[$population]</li>
-        <li><Image.scr= "flags.png">Nome:</strong>[$name.commom]</li>
-    </ul>';
-
+if(data.length>0){
+    const country = data[0];
+    const {name, capital, region, population, flags} = country;
+    countryInfoContainer.innerHTML =` 
+    <h2>Informações do País</h2> 
+    <ul>
+        <li><strong>Nome:</strong>${name.common}</li>
+        <li><strong>capital:</strong>${capital}</li>
+        <li><strong>Região:</strong>${region}</li>
+        <li><strong>População:</strong>${population}</li>
+        <li><img src='${flags.png}'/></li>
+    </ul>
+    `;
+}
 else {countryInfoContainer.innerHTML = 'Nenhum país encontrado'};
 
 });
